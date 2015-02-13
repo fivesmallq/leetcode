c3c3ea41336a487dd94c73d28cca79751a44f88d
joyjy <joyjy2ah@gmail.com>
2015-02-13 20:54:04 +0800

    001-joyjy-accepted

---
+++ 001-two-sum/joyjy/Solution.java
@@ -0,0 +1,26 @@
+/*
+ * 16 / 16 test cases passed.
+ * Status: Accepted
+ * Runtime: 241 ms
+ */
+
+public class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int length = numbers.length;
+
+        java.util.HashMap<Integer, Integer> map = new java.util.HashMap<Integer, Integer>(length);
+
+        for(int i=0; i<length;i++){
+            map.put(numbers[i], i);
+        }
+
+        for(int i=0; i<length;i++){
+            int another = target - numbers[i];
+            Integer anotherPos = map.get(another);
+            if(anotherPos != null && anotherPos > i) {
+                return new int[]{i+1, anotherPos+1};
+            }
+        }
+        return null;
+    }
+}
